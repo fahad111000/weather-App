a01033e1098261d935328293f5c21b68b58ac3b1
fahad111000 <fahad.kh2210@gmail.com>
2025-02-13 18:49:31 +0500

    Creating layout and fetch weather API

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-const apiKey = '02cfede069404b566de2a42ce9c6d658'; // Replace with your OpenWeatherMap API key
-const weatherInfo = document.getElementById("weatherInfo");
-const cityNameElement = document.getElementById("cityName");
-const temperatureElement = document.getElementById("temperature");
-const descriptionElement = document.getElementById("description");
-const humidityElement = document.getElementById("humidity");
-const getWeatherBtn = document.getElementById("getWeatherBtn");
+let searchButton = document.getElementById('searchButton');
+let weatherInfo = document.getElementById('weatherInfo');
+let cityName = document.getElementById('cityName');
+let temperature = document.getElementById('temperature');
+let description = document.getElementById('description');
+let humidity = document.getElementById('humidity');
 
-getWeatherBtn.addEventListener("click", () => {
-    const city = document.getElementById("city").value;
-    if (city) {
-        getWeather(city);
-    } else {
-        alert("Please enter a city name.");
+searchButton.addEventListener('click', () => {
+    let city = document.getElementById('city').value;
+    if (!city) {
+        return;
+    }
+
+    else {
+        getWeather(city)
     }
 });
 
-function getWeather(city) {
+async function getWeather(city) {
+    const apiKey = "02cfede069404b566de2a42ce9c6d658"; // MY API
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.cod === "404") {
-                alert("City not found. Please try again.");
-            } else {
-                displayWeather(data);
-            }
-        })
-        .catch(error => {
-            alert("Error fetching weather data.");
-        });
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error("City Not Found");
+        }
+
+        const data = await response.json();
+
+        displayWeather(data)
+    }
+
+    catch (error) {
+        alert("Error fetching weather data: " + error.message);
+
+    }
+
+
 }
 
 function displayWeather(data) {
-    weatherInfo.style.display = "block";
-    cityNameElement.textContent = `${data.name}, ${data.sys.country}`;
-    temperatureElement.textContent = `Temperature: ${data.main.temp}°C`;
-    descriptionElement.textContent = `Description: ${data.weather[0].description}`;
-    humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+    weatherInfo.style.display = 'block';
+    cityName.textContent = ` ${data.name}, ${data.sys.country}`
+    temperature.textContent = `Temperature: ${data.main.temp}°C`;
+    description = `Description: ${data.weather[0].description}`;
+    humidity = `Humidity: ${data.main.humidity}%`;
 }
